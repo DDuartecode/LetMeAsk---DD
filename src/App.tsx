1ac8fbbdd7dc9177c09244199ee6952e4ef6f2d0
dduartecode <duartesx86@gmail.com>
2021-09-14 17:53:11 -0300

    redirect e criação de salas - registro no firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
 
 
-/*exact é um componet que especifica que só pode acessar a rota
+/*exact é um componente que especifica que só pode acessar a rota
 se a rota digitada for exatamente a que está no atributo path*/
+/*{Componente Switch} não deixa chamar mais de uma rota ao mesmo tempo*/
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
